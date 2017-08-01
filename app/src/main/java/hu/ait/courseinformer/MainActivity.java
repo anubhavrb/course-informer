57c1b3bef27ff28d74a3d0422955ed4f8655f2d0
anubhavrb <anubhavrb@gmail.com>
2017-08-01 02:09:39 -0400

    Add SMS capability to the app

--- app/src/main/java/hu/ait/courseinformer/MainActivity.java
+++ app/src/main/java/hu/ait/courseinformer/MainActivity.java
@@ -1,13 +1,21 @@
 package hu.ait.courseinformer;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,6 +44,8 @@ public class MainActivity extends AppCompatActivity implements ResultListener {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        requestPermissions();
     }
 
     @OnClick(R.id.btnRequest)
@@ -66,6 +76,8 @@ public class MainActivity extends AppCompatActivity implements ResultListener {
 
     public void resultArrived(String result) {
         tvInfo.setText(result);
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("7047779769", null, result.substring(0, 20), null, null);
     }
 
     private boolean checkInputs() {
@@ -78,5 +90,30 @@ public class MainActivity extends AppCompatActivity implements ResultListener {
             return false;
         }
         return true;
+    }
+
+    private void requestPermissions() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
+                    101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case 101:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permissions granted!", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else {
+                    Toast.makeText(this, "Permissions denied.", Toast.LENGTH_SHORT)
+                            .show();
+                }
+        }
     }
 }
