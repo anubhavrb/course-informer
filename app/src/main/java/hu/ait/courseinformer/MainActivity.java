8d966e99c764cb72d070e4bf7ea09096db7c2681
anubhavrb <anubhavrb@gmail.com>
2017-07-31 22:33:25 -0400

    Add Jsoup library and ability to retrieve seat information based on department and course number

--- app/src/main/java/hu/ait/courseinformer/MainActivity.java
+++ app/src/main/java/hu/ait/courseinformer/MainActivity.java
@@ -2,12 +2,66 @@ package hu.ait.courseinformer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import hu.ait.courseinformer.network.ParseURL;
+import hu.ait.courseinformer.network.ResultListener;
+
+public class MainActivity extends AppCompatActivity implements ResultListener {
+
+    @BindView(R.id.etDep)
+    EditText etDep;
+    @BindView(R.id.etNum)
+    EditText etNum;
+    @BindView(R.id.tvInfo)
+    TextView tvInfo;
+    @BindView(R.id.btnRequest)
+    Button btnRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnRequest)
+    public void requestClicked() {
+        if (checkInputs()) {
+            tvInfo.setText("Retrieving...");
+            String dep = etDep.getText().toString().toLowerCase();
+            String num = etNum.getText().toString();
+            String URL = "http://www.davidson.edu/offices/registrar/schedules-and-courses/fall-2017-courses/"
+                        + dep + "-fall-2017-courses";
+            (new ParseURL(MainActivity.this)).execute(new String[]{URL, num});
+        }
+    }
+
+    public void resultArrived(String result) {
+        tvInfo.setText(result);
+    }
+
+    private boolean checkInputs() {
+        if (TextUtils.isEmpty(etDep.getText())) {
+            etDep.setError("Should not be empty");
+            return false;
+        }
+        if (TextUtils.isEmpty(etNum.getText())) {
+            etNum.setError("Should not be empty");
+            return false;
+        }
+        return true;
     }
 }
