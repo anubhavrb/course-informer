650f354adf0156a1b5aea3922663b6e3d5d72147
anubhavrb <anubhavrb@gmail.com>
2017-08-14 23:14:55 -0400

    Initialize Realm

--- app/src/main/java/hu/ait/courseinformer/MainActivity.java
+++ app/src/main/java/hu/ait/courseinformer/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements ResultListener {
         switch(item.getItemId()) {
             case R.id.view_courses:
                 break;
-            
+
             case R.id.add_course:
                 break;
 
